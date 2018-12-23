b61aec81f49385579676cf039b5fb60bd44de09a
Dayana Veliyeva <dayanaveliyeva@dayanas-mbp.home>
2018-12-23 16:02:44 -0500

    Initial Commit

---
+++ FinalCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FinalCalculator
+//
+//  Created by Dayana Veliyeva on 12/23/18.
+//  Copyright © 2018 Dayana Veliyeva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
