107acf420d8be851e9028ca57cadd3eee570e6fc
Dayana Veliyeva <dayanaveliyeva@Dayanas-MBP.home>
2018-12-25 00:38:59 -0500

    Intitial commit

--- FinalCalculator/ViewController.swift
+++ FinalCalculator/ViewController.swift
@@ -10,11 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Sets the title in the Navigation Bar
+        self.title = "Tip Calculator"
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        billField.becomeFirstResponder()
+    }
+    
+    
+    
+    
+    @IBAction func onTap(_ sender: AnyObject) {
+        view.endEditing(true)
+    }
+    @IBAction func calculateTip(_ sender: AnyObject) {
+      
+        let tipPercentages = [ 0.18, 0.2, 0.25]
+        
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
 
 }
+
 
--- FinalCalculatorUITests/FinalCalculatorUITests.swift
+++ FinalCalculatorUITests/FinalCalculatorUITests.swift
@@ -30,5 +30,7 @@ class FinalCalculatorUITests: XCTestCase {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
+    
+    
 
 }
